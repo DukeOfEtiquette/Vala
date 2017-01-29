8dc20e69e6fc92f1948f777e7531080b4fd70760
Adam DuQuette <duquetteadam@gmail.com>
2017-01-28 22:52:25 -0800

    Splash view is working without it's model for now

--- splash/views.py
+++ splash/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.template import loader
+
+
+def index(request):
+    #entry_list = ValaEntry.objects.order_by('creationDate')
+    template = loader.get_template('splash/index.html')
+    context = {'entry_list': ""}
+    return render(request, template, context)
