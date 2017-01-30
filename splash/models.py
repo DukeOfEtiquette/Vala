2a3eb914721ce8299e4fbbb6cafaad253d7c05df
octesian <octesian@protonmail.com>
2017-01-30 12:36:53 -0800

    Removed model class prefix, enabled unordered declaration, split index template into base and index, created class-based view for splash

--- splash/models.py
+++ splash/models.py
@@ -1,51 +1,50 @@
 from django.db import models
 
-class ValaEntryStatus( models.Model ):
+class Status( models.Model ):
     code    = models.IntegerField()
-    string  = models.CharField( max_length=64 )
+    text    = models.CharField( max_length=64 )
 
-class ValaEntryExperimentType( models.Model ):
+class ExperimentType( models.Model ):
     code    = models.IntegerField()
-    string  = models.CharField( max_length=64 )
+    name    = models.CharField( max_length=64 )
 
 class ValaEntry( models.Model ):
     hypothesis      = models.CharField( max_length=1024 )
-    experimentType  = models.ForeignKey( ValaEntryExperimentType )
-    status          = models.ForeignKey( ValaEntryStatus )
+    experimentType  = models.ForeignKey( "ExperimentType" )
+    status          = models.ForeignKey( "Status" )
     creationDate    = models.DateTimeField( 'Creation Date' )
-    projectID       = models.CharField( max_length=16 )#
-    reviewer        = models.CharField( max_length=256 )#
+    projectID       = models.CharField( max_length=16 )#STUB
+    reviewer        = models.CharField( max_length=256 )#STUB
 
+class Scientist( models.Model ):
+    valaEntry   = models.ForeignKey( "ValaEntry" )
+    scientistID = models.CharField( max_length=256 )#STUB
 
-class ValaEntryScientist( models.Model ):
-    valaEntry   = models.ForeignKey( ValaEntry )
-    scientistID = models.CharField( max_length=256 )#
-
-class ValaEntryFileType( models.Model ):
+class FileType( models.Model ):
     code    = models.IntegerField()
-    string  = models.CharField( max_length=64 )
+    type    = models.CharField( max_length=64 )
 
-class ValaEntryFile( models.Model ):
-    valaEntry     = models.ForeignKey( ValaEntry )
-    fileType      = models.ForeignKey( ValaEntryFileType )
+class File( models.Model ):
+    valaEntry     = models.ForeignKey( "ValaEntry" )
+    fileType      = models.ForeignKey( "FileType" )
     description   = models.CharField( max_length=1024 )
-    fileID        = models.CharField( max_length=64 )#
+    fileID        = models.CharField( max_length=64 )##STUB
 
-class ValaEntryFormulation( models.Model ):
-    valaEntry     = models.ForeignKey( ValaEntry )
-    formulationID = models.CharField( max_length=256 )#
+class Formulation( models.Model ):
+    valaEntry     = models.ForeignKey( "ValaEntry" )
+    formulationID = models.CharField( max_length=256 )#STUB
 
-class ValaEntryDate( models.Model):
-    valaEntry  = models.ForeignKey( ValaEntry )
+class EntryDate( models.Model):
+    valaEntry  = models.ForeignKey( "ValaEntry" )
     start      = models.DateTimeField( 'Creation Date' )
     end        = models.DateTimeField( 'Creation Date' )
 
-class ValaEntryPageRange( models.Model):
-    valaEntry  = models.ForeignKey( ValaEntry )
+class PageRange( models.Model):
+    valaEntry  = models.ForeignKey( "ValaEntry" )
     start      = models.DateTimeField( 'Creation Date' )
     end        = models.DateTimeField( 'Creation Date' )
-    notebookID = models.CharField( max_length=256 ) #
+    notebookID = models.CharField( max_length=256 ) #STUB
 
-class ValaEntryEquipment( models.Model):
-    valaEntry   = models.ForeignKey( ValaEntry )
-    equipmentID = models.CharField( max_length=256 )#
+class Equipment( models.Model):
+    valaEntry   = models.ForeignKey( "ValaEntry" )
+    equipmentID = models.CharField( max_length=256 )#STUB
