e5a241c882a460f2b3539638f5cc2a45f4f65ccc
octesian <octesian@protonmail.com>
2017-03-06 10:14:26 -0800

    Added regex validator for formID

--- splash/forms.py
+++ splash/forms.py
@@ -1,5 +1,11 @@
 from django import forms
+from django.core.validators import RegexValidator
+
+project_id_validator = RegexValidator(
+                regex='^[a-zA-Z]{2}-[0-9]+',
+                message='Hashtag doesnt comply',
+            )
 
 class NewProject(forms.Form):
-  project_id = forms.CharField(label='Project ID', max_length= 20)
+  project_id = forms.CharField(label='Project ID', max_length= 20, validators=[project_id_validator])
 
