d9caab024603fb0005487835d04bc63cba9219e2
Adam DuQuette <duquetteadam@gmail.com>
2017-04-03 14:20:16 -0700

    Created basic modelForm for ExperimentDetails

--- splash/forms.py
+++ splash/forms.py
@@ -5,18 +5,33 @@ class NewProject(forms.Form):
 
 from django.core.exceptions import ValidationError
 
-from models import ValaEntry, Equipment
+from models import ValaEntry, Equipment, ExperimentDetails
 
 class ValaEntryForm(forms.ModelForm):
   class Meta:
     model = ValaEntry
     fields = '__all__'
 
+
+class ExperimentDetsForm(forms.ModelForm):
+  class Meta:
+    model = ExperimentDetails
+    fields = ['hypothesis', 'experimentType']
+
+  def clean_hypothesis(self):
+    return self.cleaned_data['hypothesis']
+
+  def clean_experimentType(self):
+    return self.cleaned_data['experimentType']
+
+
+
+
+
 class EquipmentForm(forms.ModelForm):
   class Meta:
     model = Equipment
     fields = ['equipmentID', 'name']
-
 
   def clean_valaEntry(self):
     return self.cleaned_data['valaEntry']
