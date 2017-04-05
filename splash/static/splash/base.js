a036009da1d4ba988eeff24be32314d0e2e988e2
peacoces <macmille@oregonstate.edu>
2017-04-05 14:05:18 -0700

    resolved merge conflicts

--- splash/static/splash/base.js
+++ splash/static/splash/base.js
@@ -2,17 +2,33 @@
  * Created by Erin MacMillan on 2/11/2017.
  */
 
+$(window).on("load", function () {
 // ERIN these vars are used to validate project id length in addUser()/createProject()
-$( function() {
-    var dialog = $( "#dialog-form" ).dialog({
-        autoOpen: false,
-        height: 165,
-        width: 350,
-        modal: true,
+    $(function () {
+        var dialog = $("#dialog-form").dialog({
+            autoOpen: false,
+            height: 165,
+            width: 350,
+            modal: true,
+        });
+
+        $("#new-project").on("click", function () {
+            dialog.dialog("open");
+            return false;
+        });
     });
 
-    $( "#new-project" ).on( "click", function() {
-        dialog.dialog( "open" );
-        return false;
+    $("#id_project_id").bind('input propertychange', function () {
+        value = this.value;
+        var reg = new RegExp('^[A-z]{2}-[0-9]+$');
+        eval = value.match(reg);
+        if (eval) {
+            $('#id_project_id').css('border', '3px solid green');
+            $('#create-project').prop('disabled', false);
+        } else {
+            $('#id_project_id').css('border', '3px solid red')
+            $('#create-project').prop('disabled', true);
+        }
     });
-} );
+});
+
--- splash/static/splash/index.js
+++ splash/static/splash/index.js
@@ -4,28 +4,21 @@
 //Once document is ready...
 $(window).on("load", function () {
 
-  //Change what nav bar item is selected
-  $(".nav-bar ul li").click(function () {
-    $(".nav-bar .active").removeClass('active');
-    $(this).addClass('active');
+    //Change what nav bar item is selected
+    $(".nav-bar ul li").click(function () {
+        $(".nav-bar .active").removeClass('active');
+        $(this).addClass('active');
 
-  });
+    });
 
-  $(".click-row").click(function () {
-    val = $(this).children("span:first").text();
-    var location = "/edit/" + val;
-    window.location.href = location;
-  });
-  $("#id_project_id").bind('input propertychange', function() {
-    value = this.value;
-    var reg = new RegExp('^[A-z]{2}-[0-9]+$');
-    eval = value.match(reg);
-    if( eval ){
-      $('#id_project_id').css('border','3px solid green');
-        $('#create-project').prop('disabled', false);
-    }else{
-      $('#id_project_id').css('border','3px solid red')
-        $('#create-project').prop('disabled', true);
-    }
-  })
+    $(".click-row").click(function () {
+        val = $(this).children("span:first").text();
+        var location = "/edit/" + val;
+        window.location.href = location;
+    });
 });
+
+
+
+
+
